2a2caddd835bef331d918c6df1465ae489e4bd91
nthnn <nathanneisip@gmail.com>
2023-03-05 21:43:43 +0800

    Fixed index animation

--- assets/scripts/pg-index.js
+++ assets/scripts/pg-index.js
@@ -1,15 +1,23 @@
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
+  var reveals = document.querySelectorAll(".reveal");
 
-    for(var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
+  for(var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
 
-        if (elementTop < windowHeight - elementVisible)
-          reveals[i].classList.add("active");
-        else reveals[i].classList.remove("active");
-    }
+    if (elementTop < windowHeight - elementVisible)
+      reveals[i].classList.add("active");
+    else reveals[i].classList.remove("active");
+  }
 }
 
 window.addEventListener("scroll", reveal);
+
+let navs = ["solution", "about-us", "portfolio", "our-team"];
+for(let nav of navs) {
+  $("#" + nav).hover(()=> {
+    $(".nav-link.active").removeClass("active");  
+    $("#" + nav + "-nav").addClass("active");
+  });
+}
