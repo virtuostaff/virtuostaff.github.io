046456917e43d5afcad61acf25481337653ae8e4
nthnn <nathanneisip@gmail.com>
2023-03-07 16:19:09 +0800

    Fixed animations.

--- assets/scripts/pg-index.js
+++ assets/scripts/pg-index.js
@@ -4,7 +4,7 @@ window.addEventListener("scroll", ()=> {
   for(var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
+    var elementVisible = 200;
 
     if (elementTop < windowHeight - elementVisible)
       reveals[i].classList.add("active");
